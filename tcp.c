09845d429e133faf833d1d9e4b1ec767808e615a
ALHNk <khamidulla.alikhan@gmail.com>
2025-10-28 19:38:47 +0500

    Vse GG

--- tcp.c
+++ tcp.c
@@ -1,33 +1,104 @@
-#include <arpa/inet.h>
-#include <sys/socket.h>
-#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 #include <fcntl.h>
+#include <unistd.h>
+#include <sys/ioctl.h>
+#include <sys/mman.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <linux/videodev2.h>
 
+#define PORT 12345
+#define WIDTH 640
+#define HEIGHT 480
+#define BUFFER_COUNT 4
+
+struct buffer {
+    void   *start;
+    size_t  length;
+};
 
 int main() {
+    int fd = open("/dev/video2", O_RDWR);
+    if (fd < 0) { perror("open video"); return 1; }
+
+    struct v4l2_format fmt = {0};
+    fmt.type = V4L2_BUF_TYPE_VIDEO_CAPTURE;
+    fmt.fmt.pix.width = WIDTH;
+    fmt.fmt.pix.height = HEIGHT;
+    fmt.fmt.pix.pixelformat = V4L2_PIX_FMT_MJPEG;
+    if (ioctl(fd, VIDIOC_S_FMT, &fmt) < 0) { perror("VIDIOC_S_FMT"); return 1; }
+
+    struct v4l2_requestbuffers req = {0};
+    req.count = BUFFER_COUNT;
+    req.type = V4L2_BUF_TYPE_VIDEO_CAPTURE;
+    req.memory = V4L2_MEMORY_MMAP;
+    if (ioctl(fd, VIDIOC_REQBUFS, &req) < 0) { perror("VIDIOC_REQBUFS"); return 1; }
+
+    struct buffer *buffers = calloc(req.count, sizeof(*buffers));
+    for (int i = 0; i < req.count; i++) {
+        struct v4l2_buffer buf = {0};
+        buf.type = V4L2_BUF_TYPE_VIDEO_CAPTURE;
+        buf.memory = V4L2_MEMORY_MMAP;
+        buf.index = i;
+        if (ioctl(fd, VIDIOC_QUERYBUF, &buf) < 0) { perror("VIDIOC_QUERYBUF"); return 1; }
+        buffers[i].length = buf.length;
+        buffers[i].start = mmap(NULL, buf.length, PROT_READ | PROT_WRITE, MAP_SHARED, fd, buf.m.offset);
+        if (buffers[i].start == MAP_FAILED) { perror("mmap"); return 1; }
+    }
+
+    for (int i = 0; i < req.count; i++) {
+        struct v4l2_buffer buf = {0};
+        buf.type = V4L2_BUF_TYPE_VIDEO_CAPTURE;
+        buf.memory = V4L2_MEMORY_MMAP;
+        buf.index = i;
+        if (ioctl(fd, VIDIOC_QBUF, &buf) < 0) { perror("VIDIOC_QBUF"); return 1; }
+    }
+
+    int type = V4L2_BUF_TYPE_VIDEO_CAPTURE;
+    if (ioctl(fd, VIDIOC_STREAMON, &type) < 0) { perror("VIDIOC_STREAMON"); return 1; }
+
+    /* TCP server setup */
     int server_fd = socket(AF_INET, SOCK_STREAM, 0);
+    if (server_fd < 0) { perror("socket"); return 1; }
+
+    int opt = 1;
+    setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt));
+
     struct sockaddr_in addr = {0};
     addr.sin_family = AF_INET;
-    addr.sin_port = htons(8080);
     addr.sin_addr.s_addr = INADDR_ANY;
-    bind(server_fd, (struct sockaddr*)&addr, sizeof(addr));
-    listen(server_fd, 1);
-    int client = accept(server_fd, NULL, NULL);
+    addr.sin_port = htons(PORT);
 
-    int fd = open("/dev/video2", O_RDWR);
-    unsigned char buf[1024*1024];
-    char *header = 
-"HTTP/1.0 200 OK\r\n"
-"Content-Type: multipart/x-mixed-replace; boundary=frame\r\n\r\n";
-write(client, header, strlen(header));
+    if (bind(server_fd, (struct sockaddr*)&addr, sizeof(addr)) < 0) { perror("bind"); return 1; }
+    if (listen(server_fd, 1) < 0) { perror("listen"); return 1; }
 
-while (1) {
-    int len = read(fd, buf, sizeof(buf));
-    if (len > 0) {
-        dprintf(client, "--frame\r\nContent-Type: image/jpeg\r\nContent-Length: %d\r\n\r\n", len);
-        send(client, buf, len, 0);
-        send(client, "\r\n", 2, 0);
+    printf("Waiting for connection on port %d...\n", PORT);
+    int client_fd = accept(server_fd, NULL, NULL);
+    if (client_fd < 0) { perror("accept"); return 1; }
+    printf("Client connected, streaming MJPEG...\n");
+
+    while (1) {
+        struct v4l2_buffer buf = {0};
+        buf.type = V4L2_BUF_TYPE_VIDEO_CAPTURE;
+        buf.memory = V4L2_MEMORY_MMAP;
+        if (ioctl(fd, VIDIOC_DQBUF, &buf) < 0) { perror("VIDIOC_DQBUF"); break; }
+
+        ssize_t sent = send(client_fd, buffers[buf.index].start, buf.bytesused, MSG_NOSIGNAL);
+        if (sent < 0) { perror("send"); break; }
+
+        if (ioctl(fd, VIDIOC_QBUF, &buf) < 0) { perror("VIDIOC_QBUF"); break; }
     }
+
+    ioctl(fd, VIDIOC_STREAMOFF, &type);
+    for (int i = 0; i < req.count; i++)
+        munmap(buffers[i].start, buffers[i].length);
+    free(buffers);
+    close(client_fd);
+    close(server_fd);
+    close(fd);
+    return 0;
 }
 
-}
