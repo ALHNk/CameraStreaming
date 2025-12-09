668260e169137c138f0fec3144ac48b0b3b18149
ALHNk <khamidulla.alikhan@gmail.com>
2025-12-09 08:45:51 +0500

    TwoCameras But 1 is not working

--- main.c
+++ main.c
@@ -1,49 +1,61 @@
 #include <stdio.h>
-#include <string.h>
+#include <stdlib.h>
+#include <pthread.h>
 #include <unistd.h>
 #include "camera.h"
 #include "udp.h"
 
-// #define SECRET "Camera"
-#define DEFAULT_PORT 5000
-#define DEFAULT_DEVICE "/dev/video2"
-
-int main(int argc, char *argv[]) {
-    char *device;
+struct cam_thread_arg {
+    const char *device;
     int port;
-    if(argc < 3)
-    {
-        device =  DEFAULT_DEVICE;
-        port = DEFAULT_PORT;
-    }
-    else 
-    {
-        device =  argv[1];
-        port = atoi(argv[2]);
-    }
-    int fd = camera_open(device);
-    if (fd < 0) return 1;
+};
+
+void *camera_thread(void *arg) {
+    struct cam_thread_arg *cfg = arg;
+
+    int fd = camera_open(cfg->device);
+    if (fd < 0) pthread_exit(NULL);
 
     struct buffer *buffers;
     int buffer_count;
-    if (camera_start(fd, &buffers, &buffer_count) < 0) return 1;
+    if (camera_start(fd, &buffers, &buffer_count) < 0) pthread_exit(NULL);
 
-    int sockfd = udp_init(port);
-    if (sockfd < 0) return 1;
-
-    printf("Streaming started (UDP)...\n");
+    struct udp_sender sender = udp_init(cfg->port);
 
     void *frame;
     size_t size;
+
     while (1) {
         if (camera_capture(fd, buffers, buffer_count, &frame, &size) == 0) {
-            printf("%ld \n", size);
-            udp_send(sockfd, frame, size);
+            udp_send(&sender, frame, size);
         }
-        usleep(1000); // reduce CPU load
+        usleep(1000);
     }
 
     camera_release(fd, buffers, buffer_count);
-    udp_close(sockfd);
+    udp_close(&sender);
+
+    return NULL;
+}
+
+int main(int argc, char *argv[]) {
+    pthread_t t1, t2;
+
+    struct cam_thread_arg cam1 = { "/dev/video2", 5000 };
+    struct cam_thread_arg cam2 = { "/dev/video4", 5001 };
+
+    if (argc == 5) {
+        cam1.device = argv[1];
+        cam1.port = atoi(argv[2]);
+        cam2.device = argv[3];
+        cam2.port = atoi(argv[4]);
+    }
+
+    pthread_create(&t1, NULL, camera_thread, &cam1);
+    pthread_create(&t2, NULL, camera_thread, &cam2);
+
+    pthread_join(t1, NULL);
+    pthread_join(t2, NULL);
+
     return 0;
 }
--- udp.h
+++ udp.h
@@ -2,9 +2,15 @@
 #define UDP_H
 
 #include <stddef.h>
+#include <arpa/inet.h>
 
-int udp_init(int port);
-int udp_send(int sockfd, const void *data, size_t size);
-void udp_close(int sockfd);
+struct udp_sender {
+    int sockfd;
+    struct sockaddr_in addr;
+};
+
+struct udp_sender udp_init(int port);
+int udp_send(struct udp_sender *u, const void *data, size_t size);
+void udp_close(struct udp_sender *u);
 
 #endif
