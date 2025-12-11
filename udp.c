a3f44bfec6eb3e961e13fd1e3a1c7bac269ce3cf
ALHNk <khamidulla.alikhan@gmail.com>
2025-12-11 11:02:52 +0500

    Increaseing UDP socket buffer

--- udp.c
+++ udp.c
@@ -3,6 +3,7 @@
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
+#include <sys/socket.h>
 
 
 struct udp_sender udp_init(int port) {
@@ -12,6 +13,24 @@ struct udp_sender udp_init(int port) {
 
     int opt = 1;
     setsockopt(u.sockfd, SOL_SOCKET, SO_BROADCAST, &opt, sizeof(opt));
+
+    // INCREASE SEND BUFFER SIZE
+    int sndbuf = 1024 * 1024; // 1MB buffer
+    if (setsockopt(u.sockfd, SOL_SOCKET, SO_SNDBUF, &sndbuf, sizeof(sndbuf)) < 0) {
+        perror("setsockopt SO_SNDBUF");
+    }
+    
+    // SET PRIORITY (helps with Wi-Fi scheduling)
+    int priority = 6;
+    if (setsockopt(u.sockfd, SOL_SOCKET, SO_PRIORITY, &priority, sizeof(priority)) < 0) {
+        perror("setsockopt SO_PRIORITY");
+    }
+    
+    // DISABLE NAGLE-LIKE BEHAVIOR
+    int nodelay = 1;
+    if (setsockopt(u.sockfd, IPPROTO_IP, IP_TOS, &nodelay, sizeof(nodelay)) < 0) {
+        perror("setsockopt IP_TOS");
+    }
 
     memset(&u.addr, 0, sizeof(u.addr));   
     u.addr.sin_family = AF_INET;
